df7e615e8b7f1c44b06d36d0810683f71fe9f81d
Emilia Tulan <emilia.tulan@gmail.com>
2021-04-11 20:48:57 +0200

    fix error getserversideprops

--- pages/login.tsx
+++ pages/login.tsx
@@ -4,6 +4,7 @@ import {
   useState,
 } from 'react';
 
+import { GetServerSidePropsContext } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
